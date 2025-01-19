25f16a8f6a11e5c9003e02c0806662b16a49be79
minkyung kim <minry20415@naver.com>
2025-01-19 15:16:21 +0900

    ✨: JPA, QueryDSL 환경 설정

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,41 @@
 package study.querydsl;
 
+import static org.assertj.core.api.Assertions.*;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 @SpringBootTest
+@Transactional
+@Commit
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+
+//		QHello qHello = new QHello("h");
+		QHello qHello = QHello.hello;
+
+		Hello result = query
+			.selectFrom(qHello)
+			.fetchOne();
+
+		assertThat(result).isEqualTo(hello); // queryDSL 테스트
+		assertThat(result.getId()).isEqualTo(hello.getId()); // lombok 테스트
 	}
 
 }
